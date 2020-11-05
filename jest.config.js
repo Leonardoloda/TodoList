bb16b4d251914a0d00cff0063e8c1f4add7c6805
Leonardo <leonardoloda2@gmail.com>
2020-11-04 21:54:38 -0500

    fix: ignore e2e tests from jest configuration

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@ module.exports = {
     "^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
       "jest-transform-stub",
   },
+  modulePaths: ["<rootDir>/src/"],
+  modulePathIgnorePatterns: ["<rootDir>/e2e/"],
   transform: {
     "^.+\\.js$": "babel-jest",
     "^.+\\.vue$": "vue-jest",
